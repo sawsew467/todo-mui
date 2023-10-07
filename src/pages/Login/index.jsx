716968db9b264d1666a5cbcba71669d854ec933f
sawsew467 <bao.thang.1912@gmail.com>
2023-10-07 20:02:27 +0700

    login form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,32 @@
-import { Box, Button, Card, CardContent, TextField, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Login() {
+  const [inputValues, setInputValues] = useState({
+    username: "admin",
+    password: "123456",
+  });
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (inputValues.username === "admin" && inputValues.password === "123456") {
+        toast.success("Login successfully!!!")
+        navigate('/home')
+    } else {
+        toast.error("Invalid username or password")
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -15,28 +40,46 @@ function Login() {
       }}
     >
       <Card sx={{ minWidth: 600 }}>
-        <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <Typography variant="h4" textAlign={"center"} marginBottom={2}>
             Login
           </Typography>
-          <TextField fullWidth
+          <TextField
+            fullWidth
             id="outlined-basic"
             label="Username"
             variant="outlined"
             sx={{ flex: 3 }}
           />
-          <TextField fullWidth
+          <TextField
+            fullWidth
             id="outlined-basic"
             label="Password"
             variant="outlined"
             sx={{ flex: 3, marginTop: 2 }}
           />
-          <Button variant="contained" fullWidth sx={{my: 2}}>Login</Button>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ my: 2 }}
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
           <Typography variant="body" textAlign={"center"}>
             Don't have account?
           </Typography>
-          <Link to={'/register'}>fewfewfew</Link>
-          <Button variant="outlined" fullWidth sx={{mt: 2}}>Register</Button>
+          <Link to={"/register"} style={{ width: "100%" }}>
+            <Button variant="outlined" fullWidth sx={{ mt: 2 }}>
+              Register
+            </Button>
+          </Link>
         </CardContent>
       </Card>
     </Box>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,4 +1,12 @@
-import { Box, Button, Card, CardContent, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
@@ -14,27 +22,41 @@ function Register() {
       }}
     >
       <Card sx={{ minWidth: 600 }}>
-        <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <Typography variant="h4" textAlign={"center"} marginBottom={2}>
             Register
           </Typography>
-          <TextField fullWidth
+          <TextField
+            fullWidth
             id="outlined-basic"
             label="Username"
             variant="outlined"
             sx={{ flex: 3 }}
           />
-          <TextField fullWidth
+          <TextField
+            fullWidth
             id="outlined-basic"
             label="Password"
             variant="outlined"
             sx={{ flex: 3, marginTop: 2 }}
           />
-          <Button variant="contained" fullWidth sx={{my: 2}}>Register</Button>
+          <Button variant="contained" fullWidth sx={{ my: 2 }}>
+            Register
+          </Button>
           <Typography variant="body" textAlign={"center"}>
             Have account?
           </Typography>
-          <Button variant="outlined" fullWidth sx={{mt: 2}}>Login</Button>
+          <Link to={"/login"} style={{ width: "100%" }}>
+            <Button variant="outlined" fullWidth sx={{ mt: 2 }}>
+              Login
+            </Button>
+          </Link>
         </CardContent>
       </Card>
     </Box>
